697e50857ea26f79178b937f554d5aff84709801
Lacaze Robin <robin.lacaze@student-cs.fr>
2026-09-07 15:22:55 +0200

    feat(selection-verty): fixing background blur

--- assets/selection-signup.js
+++ assets/selection-signup.js
@@ -332,8 +332,6 @@
       overlay.addEventListener('click', () => this.close());
       this.root.appendChild(overlay);
 
-      var scroll = el('div', { class: 'vs-modal__scroll' });
-
       var page = el('div', { class: 'vs-modal__page' });
       var col = el('div', { class: 'vs-modal__col' });
 
@@ -468,8 +466,7 @@
       actionbar.appendChild(actionRow);
       page.appendChild(actionbar);
 
-      scroll.appendChild(page);
-      this.root.appendChild(scroll);
+      this.root.appendChild(page);
     }
   }
 
